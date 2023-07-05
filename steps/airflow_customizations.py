3e4c6899be9513b75777a42ce2634266d2d329d1
Alison <31246169+aapeebles@users.noreply.github.com>
2023-07-04 22:06:32 -0400

    Create airflow_customizations.py

---
+++ steps/airflow_customizations.py
@@ -0,0 +1,7 @@
+## Customizations
+
+[code editor](https://github.com/andreax79/airflow-code-editor)
+[black formatter](https://github.com/psf/black)
+
+`pip install black==21.12b0`
+`pip install airflow-code-editor==7.3.0`
